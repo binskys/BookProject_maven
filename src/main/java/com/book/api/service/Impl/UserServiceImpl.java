2a87968030f4cc8d096ad3eb4e2bb364737933e7
binskys <1023342952@qq.com>
2017-10-12 10:19:56 +0800

    可以了。数据库查询不行

--- src/main/java/com/book/api/service/Impl/UserServiceImpl.java
+++ src/main/java/com/book/api/service/Impl/UserServiceImpl.java
@@ -6,12 +6,19 @@ import com.book.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("UserServiceImpl")
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao user;
 
     public User queryByAccount(String account) {
-        return user.queryByAccount(account);
+
+        User user2=new User();
+        user2.setAccount("18170465691");
+        user2.setPassword("a123456");
+
+        return user2;
+       // return user.queryByAccount(account);
+
     }
 }
